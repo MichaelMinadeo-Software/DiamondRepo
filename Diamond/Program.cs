2109c4b283885405d4cd22bab8f33f582aff9547
Michael Minadeo <michael.minadeo@rrd.com>
2019-08-13 07:40:36 -0500

    Add project files.

---
+++ Diamond/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter middle Character: ");
+            var c = char.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            DiamondCreator.GenerateDiamond(c);
+
+        }
+    }
+}
